545da06fec0b3d8a304c54b54bccff8581be3cb1
kritschy <christopher.lux@schueler.sfn-kassel.de>
2020-02-04 19:14:43 +0100

    add afd-collector

---
+++ backend/collectors/afd/afd.py
@@ -0,0 +1,31 @@
+import feedparser as fd
+import time
+import json as js
+import re
+
+dateofLastEntry = 0
+
+
+feed = fd.parse("https://www.afdbundestag.de/feed/")
+entry = feed.entries[0]
+entry_published = time.mktime(entry['published_parsed'])
+if entry_published > dateofLastEntry:
+    print(entry_published)
+
+    text = js.dumps(entry.content[0].value, indent="\t", ensure_ascii=False).encode('utf-8').decode()
+    text = re.sub(r'<.*?>', '', text).replace('\\n', ' ')
+
+
+    content = {
+
+        "title": entry['title'],
+        "published": entry_published,
+        "summary": entry['summary'],
+        "text": text
+
+
+    }
+
+data_File = open("output.txt", "w")
+data_File.write(str(text))
+data_File.close()
